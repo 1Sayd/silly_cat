aaa087d21af79226724e7a1a9d8c149f7b8d443f
Denys Chmil <person.tak@gmail.com>
2023-05-15 21:47:31 +0300

    S

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(compression());
-app.use(static(join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`{App listening on port ${port}`)
